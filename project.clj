6bd542319b906be6ad62299fb915d4d7a7d24c75
nepping <n.epping@evosec.de>
2019-09-30 15:15:35 +0200

    Fix uberjar parameters

--- project.clj
+++ project.clj
@@ -9,9 +9,8 @@
    {:dependencies [[metabase-core "1.0.0-SNAPSHOT"]]}
 
    :uberjar
-   {:auto-cleam     true
+   {:auto-clean     true
     :aot            :all
-    :omit-source    true
     :javac-options  ["-target" "1.8", "-source" "1.8"]
     :target-path    "target/%s"
     :uberjar-name   "firebird.metabase-driver.jar"}})
